d004f42e41cba5d77f5e7f320cfb4c2623685bce
diasantoine <61905755+diasantoine@users.noreply.github.com>
2022-10-22 16:43:34 +0200

    Mise En place Code C++

---
+++ Intermediate/Build/Win64/MagnetCarsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.cpp
@@ -0,0 +1,2 @@
+// Compiler: 14.29.30133
+#include "D:/Ican/Cour ican/Ican Seconde Annee Mastere/ProjetPremierTrimestre/MagnetCars/Intermediate/Build/Win64/MagnetCarsEditor/Development/Engine/SharedPCH.Engine.ShadowErrors.h"
---
+++ Source/MagnetCars/Public/GMGravityCars.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "GMGravityCars.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MAGNETCARS_API AGMGravityCars : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public :
+	UPROPERTY(BlueprintReadWrite,EditAnywhere)
+	int Bonsoir = 5;
+	
+};
