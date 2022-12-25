247dbda4f426d0c249b4305a4d9648efbd37a69a
comte-67 <93796224+comte-67@users.noreply.github.com>
2022-12-26 00:41:20 +0100

    Build correction 1ère étape

--- Intermediate/Build/Win64/UnrealEditor/Inc/MagnetCars/MagnetCars.init.gen.cpp
+++ Intermediate/Build/Win64/UnrealEditor/Inc/MagnetCars/MagnetCars.init.gen.cpp
@@ -17,13 +17,13 @@ void EmptyLinkFunctionForGeneratedCodeMagnetCars_init() {}
 				nullptr,
 				0,
 				PKG_CompiledIn | 0x00000000,
-				0xB8E8DC60,
-				0x32340D72,
+				0xF9BE1795,
+				0x681CF6D2,
 				METADATA_PARAMS(nullptr, 0)
 			};
 			UECodeGen_Private::ConstructUPackage(Z_Registration_Info_UPackage__Script_MagnetCars.OuterSingleton, PackageParams);
 		}
 		return Z_Registration_Info_UPackage__Script_MagnetCars.OuterSingleton;
 	}
-	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MagnetCars(Z_Construct_UPackage__Script_MagnetCars, TEXT("/Script/MagnetCars"), Z_Registration_Info_UPackage__Script_MagnetCars, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xB8E8DC60, 0x32340D72));
+	static FRegisterCompiledInInfo Z_CompiledInDeferPackage_UPackage__Script_MagnetCars(Z_Construct_UPackage__Script_MagnetCars, TEXT("/Script/MagnetCars"), Z_Registration_Info_UPackage__Script_MagnetCars, CONSTRUCT_RELOAD_VERSION_INFO(FPackageReloadVersionInfo, 0xF9BE1795, 0x681CF6D2));
 PRAGMA_ENABLE_DEPRECATION_WARNINGS
--- Source/MagnetCars/MagnetCars.Build.cs
+++ Source/MagnetCars/MagnetCars.Build.cs
@@ -8,7 +8,7 @@ public class MagnetCars : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EngineSettings" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
