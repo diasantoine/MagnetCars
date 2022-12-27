8ddd3631d3e2937eebfbf84bee06d37460668051
comte-67 <93796224+comte-67@users.noreply.github.com>
2022-12-27 16:50:05 +0100

    Revert "Build correction 1ère étape"
    This reverts commit 247dbda4f426d0c249b4305a4d9648efbd37a69a.

--- Source/MagnetCars/MagnetCars.Build.cs
+++ Source/MagnetCars/MagnetCars.Build.cs
@@ -8,7 +8,7 @@ public class MagnetCars : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EngineSettings" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
